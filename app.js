bbb379fdc426f3a65ffd37c0d05955e7be2bbef6
root <saschadwest@gmail.com>
2020-01-10 04:57:25 +0000

    write and upload to heroku

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require("express"),
+    app		=  express(),
+	bodyParser = require("body-parser"),
+    ejs		= require("ejs"),
+	colors = require("colors"),
+	mongoose	= require("mongoose");
+	methodOverride = require("method-override")
+
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
+
+//Configure Mongoose
+mongoose.connect("mongodb://localhost:27017/carlos", { useNewUrlParser: true,useUnifiedTopology: true });
+
+
+app.get("/",function(req,res){
+	res.redirect("/index");
+});
+
+app.get("/index",function(req,res){
+	res.render("ourstory");
+})
+
+
+
+app.listen(3000,function(){
+	console.log("Lets Make a Profile, for an old friend.".yellow)
+})
